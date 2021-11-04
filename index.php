d504193857d902275b06e61b11444ec98973098c
Kirill <kazachkov99k@gmail.com>
2021-11-04 14:42:59 +0400

    добавлена проверка на вход пользователя

--- index.php
+++ index.php
@@ -1,21 +1,13 @@
 <?php
 session_start();
+if (isset($_SESSION['logged_user'])){
 $user = $_SESSION['logged_user'];
-?>
-
-    <html>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport"
-              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="style.css">
-    </head>
-    <div class="form_div">
-    <h2>Добро пожаловать, <?php echo $user['name']; ?></h2>
-    <form method='post' action="list.php">
-        <input class="button" type='submit' name='logout' value='Просмотр новостей'>
-    </form>
-    </div>
-    </body>
-    </html>
+echo "<div class='form_div'>";
+echo "<h2>Добро пожаловать, $user[name]</h2>";
+echo "<form method='post' action='list.php'>";
+echo "<input class='button' type='submit' name='logout' value='Просмотр новостей'>";
+echo "</form></div>";
+}
+else{
+    echo 'Пожалуйста, <a href = login.html>войдите</a> в систему';
+}
